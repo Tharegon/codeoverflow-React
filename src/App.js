426664127277ee705ed6fe041d272033a73c3d0d
ZsoltTirkala <tirkala.zsolt@gmail.com>
2021-05-27 15:36:25 +0200

    Create basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useMemo } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import clsx from "clsx";
+import "./App.css";
+import Register from "./pages/Register";
+import { AuthProvider } from "./components/contexts/AuthContext";
 
 function App() {
+
   return (
-    <div className="App">
-      
-    </div>
+    <React.Fragment>
+      <BrowserRouter>
+        <AuthProvider>
+          <Switch>
+            <Route path="/register">
+              <Register />
+            </Route>
+          </Switch>
+        </AuthProvider>
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
 
